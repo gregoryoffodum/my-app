53f316b3cf71992b7609c43971a4e8e8c1076fc6
gregoryoffodum <gregoryoffodum@gmail.com>
2024-04-25 23:31:05 -0400

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '22b04711-3d27-4cd1-8785-fe12fc648ad0';
+export const MODAL_CLOSE_SEC = 2.5;
